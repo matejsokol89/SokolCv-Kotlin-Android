782685dd109c285f750a2b73d111bef065774af4
Matej Sokol <sokolvm@gmail.com>
2019-05-07 14:10:12 +0200

    rad na android cv-u

---
+++ app/src/main/java/com/example/sokolcv/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sokolcv
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mikepenz.iconics.context.IconicsContextWrapper
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
